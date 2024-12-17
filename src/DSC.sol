06bc59877805fb06c346c54f8e3bef8f6ceb62b1
samuelajayii <ajayisamuel023@gmail.com>
2024-12-17 13:33:13 +0100

    forgot to git add lol

---
+++ src/DSC.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DSC is ERC20Burnable, Ownable {
+    constructor() ERC20("MyStableCoin", "MSC") Ownable(msg.sender) {}
+
+    error AmountMustBeMoreThanZero();
+    error BurnAmountExceedsBalance();
+    error NotZeroAddress();
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
