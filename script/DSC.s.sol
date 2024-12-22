9c80437d56834beaa032f9ff0aca16de2a93017c
samuelajayii <ajayisamuel023@gmail.com>
2024-12-22 21:31:19 +0100

    added some tests and written the dsc engine

--- script/DSC.s.sol
+++ script/DSC.s.sol
@@ -5,8 +5,10 @@ import {DSC} from "../src/DSC.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract DeployDSC is Script {
+    address public constant owner = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
     function run() external returns (DSC) {
-        vm.startBroadcast();
+        vm.startBroadcast(owner);
         DSC dsc = new DSC();
         vm.stopBroadcast();
 
--- test/DSC.t.sol
+++ test/DSC.t.sol
@@ -4,9 +4,31 @@ pragma solidity ^0.8.27;
 import {Test, console} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {DSC} from "../src/DSC.sol";
+import {DeployDSC} from "../script/DSC.s.sol";
 
-contract EngineTest {
-    function setup() external {
-        DSC dsc = new DSC();
+contract EngineTest is Test {
+    DSC dsc;
+    address public owner = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address public bob = makeAddr("bob");
+
+    function setUp() external {
+        vm.prank(owner);
+        DeployDSC deployDSC = new DeployDSC();
+        dsc = deployDSC.run();
+    }
+
+    function testNameDeployment() public {
+        assertEq(keccak256(abi.encodePacked(dsc.name())), keccak256(abi.encodePacked("MyStableCoin")));
+        assertEq(keccak256(abi.encodePacked(dsc.symbol())), keccak256(abi.encodePacked("MSC")));
+    }
+
+    function testTotalSupply() public {
+        assertEq(dsc.totalSupply(), 0);
+    }
+
+    function testOnlyOwnership() public {
+        vm.expectRevert();
+        vm.prank(bob);
+        dsc.mint(bob, 200);
     }
 }
